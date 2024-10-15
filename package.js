4123ef5a0371a913f3a14887a1b2d51de09265b8
OGNINJA2221 <naqeebsead@gmail.com>
2024-10-15 17:56:29 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "Ninja-Helper",
+  "version": "0.1.0",
+  "description": "A simple Discord bot built on Node.js, instantly up and running.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.16.3"
+  },
+  "engines": {
+    "node": ">=20.18.0"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "discord"
+  ]
+}
